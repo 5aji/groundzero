3c7851138f25b982dad7ff2bcea2edab33473eb0
saji <champ189@umn.edu>
2023-02-01 16:23:57 -0600

    formatting

--- groundzero/main.py
+++ groundzero/main.py
@@ -10,15 +10,13 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__()
 
         self.setWindowTitle("groundzero ui")
-        
-        self.statusBar() # call this once to make it appear.
-        self.statusBar().showMessage("Welcome to GroundZero")
 
+        self.statusBar()  # call this once to make it appear.
+        self.statusBar().showMessage("Welcome to GroundZero")
 
         self.viewMenu = self.menuBar().addMenu("View")
 
         # set up dock widgets (properties viewer, shell, graphs, node toolbox)
-
 
 
 if __name__ == "__main__":
